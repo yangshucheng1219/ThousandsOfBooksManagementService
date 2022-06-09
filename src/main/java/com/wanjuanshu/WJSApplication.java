963dd92747db8993212ec1ffc2d7db11a159fa61
yangshucheng1219 <yangshucheng1219@foxmail.com>
2022-06-10 00:27:54 +0800

    简单注释

--- src/main/java/com/wanjuanshu/WJSApplication.java
+++ src/main/java/com/wanjuanshu/WJSApplication.java
@@ -4,6 +4,9 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 程序入口
+ */
 @SpringBootApplication
 @EnableSwagger2Doc
 public class WJSApplication {
